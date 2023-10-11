da0f4963527f2af77afd31314c04ee69e4fae292
anjchak <ths25113@towerhill.org>
2023-10-11 18:50:39 -0400

    COMMIT

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/old_driveop2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/old_driveop2.java
@@ -8,21 +8,16 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
 
 
-
-@TeleOp(name = "DriveOpMainDuocontrolOld   (Blocks to Java)")
-public class old_driveop extends LinearOpMode {
+@TeleOp(name = "DriveOpMainDuocontrol (Blocks to Java)")
+public class old_driveop2 extends LinearOpMode {
 
     private DcMotor backLeft;
     private DcMotor backRight;
     private DcMotor frontLeft;
     private DcMotor frontRight;
-    private DcMotor ArmMotor;
-    private Servo ClawRight;
-    private Servo ClawLeft;
 
     double rightPower;
     double Power;
-    double triggerPower;
     String driveMode;
     double leftPower;
 
@@ -41,20 +36,15 @@ public class old_driveop extends LinearOpMode {
         backRight = hardwareMap.get(DcMotor.class, "backRight");
         frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
         frontRight = hardwareMap.get(DcMotor.class, "frontRight");
-        ArmMotor = hardwareMap.get(DcMotor.class, "ArmMotor");
-        ClawRight = hardwareMap.get(Servo.class, "ClawRight");
-        ClawLeft = hardwareMap.get(Servo.class, "ClawLeft");
 
         backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        ArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         frontLeft.setDirection(DcMotorSimple.Direction.FORWARD);
         backRight.setDirection(DcMotorSimple.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        ClawRight.setDirection(Servo.Direction.REVERSE);
         Power = 0.7;
         waitForStart();
         if (opModeIsActive()) {
@@ -64,12 +54,6 @@ public class old_driveop extends LinearOpMode {
                 previousGamePad2.copy(currentGamePad2);
                 currentGamePad2.copy(gamepad2);
 
-                if (currentGamePad2.left_trigger != 0) {
-                    triggerPower = gamepad2.left_trigger * 0.5;
-                }
-                if (currentGamePad1.right_trigger != 0) {
-                    triggerPower = -(gamepad2.right_trigger * 1);
-                }
                 if (currentGamePad1.right_stick_x == 0) {
                     driveMode = "straight";
                     leftPower = currentGamePad1.left_stick_y * Power;
@@ -78,16 +62,7 @@ public class old_driveop extends LinearOpMode {
                     driveMode = "turn";
                     leftPower = currentGamePad1.left_stick_y * Power - gamepad1.right_stick_x * Power;
                     rightPower = currentGamePad1.left_stick_y * Power + gamepad1.right_stick_x * Power;
-                }
-                if (gamepad2.a) {
-                    ClawLeft.setPosition(0.9);
-                    ClawRight.setPosition(0.9);
-                }
-                if (gamepad2.b) {
-                    ClawLeft.setPosition(0.4);
-                    ClawRight.setPosition(0.4);
-                }
-                ArmMotor.setPower(triggerPower);
+                };
                 backLeft.setPower(leftPower);
                 frontLeft.setPower(leftPower);
                 backRight.setPower(rightPower);
@@ -95,7 +70,6 @@ public class old_driveop extends LinearOpMode {
                 telemetry.addData("Drive Mode: ", driveMode);
                 telemetry.addData("Left Power:", leftPower);
                 telemetry.addData("Right Power:", rightPower);
-                telemetry.addData("Trigger Power:", triggerPower);
                 telemetry.update();
             }
         }
