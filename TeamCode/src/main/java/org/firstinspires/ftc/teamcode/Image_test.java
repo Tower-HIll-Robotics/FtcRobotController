069dd11d1d06f84a1fb6c10d00a69daaa9bbe365
joeyzhu <joeyzhu1106@gmail.com>
2023-11-07 19:36:48 -0500

    April Tag testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Image_test.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import java.util.List;
+
+@Autonomous(name = "Image_test", preselectTeleOp = "DriveOp Main Duocontrol")
+public class Image_test {
+
+}
