6d2f2e01006bf0b74bb7706b1ba1fc133ce11684
joeyzhu <joeyzhu1106@gmail.com>
2023-11-28 19:39:44 -0500

    Recognition testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RecTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RecTest.java
@@ -47,7 +47,7 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
+@TeleOp(name = "RecTest", group = "Concept")
 public class RecTest extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
@@ -105,6 +105,7 @@ public class RecTest extends LinearOpMode {
 
         // Create the TensorFlow processor by using a builder.
         tfod = new TfodProcessor.Builder()
+                .setModelFileName("Marker")
 
                 // Use setModelAssetName() if the TF Model is built in as an asset.
                 // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
